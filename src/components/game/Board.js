dbd454af4131fda34a750c2843f84de573ee8098
meti651 <meti651@gmail.com>
2021-04-29 00:30:59 +0200

    refactor win check, implement players

--- src/components/game/Board.js
+++ src/components/game/Board.js
@@ -1,91 +1,83 @@
 import React, { useEffect, useState } from "react";
 import Cell from "./Cell";
 
-export default function Board({ width = 10, height = 10 }) {
+export default function Board({
+    width = 10,
+    height = 10,
+    players = [
+        { name: "Player 1", sign: "X" },
+        { name: "Player 2", sign: "O" },
+    ],
+}) {
     const [board, setBoard] = useState();
+    const [playerNum, setPlayerNum] = useState(0);
+    const [currentPlayer, setCurrentPlayer] = useState(players[playerNum]);
 
     useEffect(() => {
-        setBoard(Array.from({ length: height }, () => Array(width).fill(null)));
+        setBoard(Array.from({ length: height }, () => Array(width).fill(null))); // create the board matrix
     }, [width, height]);
 
+    useEffect(() => {
+        setCurrentPlayer((state) => players[playerNum]);
+    }, [playerNum]);
+
     const playerMove = (target) => {
-        const x = target.x;
-        const y = target.y;
-        board[x][y] = 1;
-        if (checkWin({ x: x, y: y })) {
-            alert("You win!");
+        board[target.x][target.y] = currentPlayer.sign;
+        const isWin =
+            checkWin(target, 0, 1) || // check column
+            checkWin(target, 1, 0) || // check row
+            checkWin(target, -1, 1) || // check / diagonal
+            checkWin(target, -1, -1); // check \ diagonal
+
+        if (isWin) {
+            alert(`${currentPlayer.name} win!`);
         }
+
+        nextPlayer();
     };
 
-    const checkWin = (startingPoint) => {
-        let inline = 1;
-        const leftSign = { x: startingPoint.x, y: startingPoint.y - 1 };
-        while (board[leftSign.x][leftSign.y]) {
-            leftSign.y -= 1;
-            inline++;
-        }
-        const rightSign = { x: startingPoint.x, y: startingPoint.y + 1 };
-        while (board[rightSign.x][rightSign.y]) {
-            rightSign.y += 1;
-            inline++;
+    const nextPlayer = () => {
+        setPlayerNum((state) => {
+            const newState = state + 1 < players.length ? state + 1 : 0;
+            return newState;
+        });
+    };
+
+    const checkWin = (startingPoint, xModifier, yModifier) => {
+        let inLine = 0;
+        const currentPoint = Object.assign({}, startingPoint);
+
+        let isInBoard = currentPoint.x >= 0 && currentPoint.x < height;
+        while (isInBoard && board[currentPoint.x][currentPoint.y] === currentPlayer.sign) {
+            currentPoint.x += xModifier;
+            currentPoint.y += yModifier;
+            isInBoard = currentPoint.x >= 0 && currentPoint.x < height;
+            inLine++;
         }
 
-        if (inline >= 5) {
-            return true;
-        }
-        inline = 1;
-        const upSign = { x: startingPoint.x - 1, y: startingPoint.y };
-        if (upSign.x >= 0) {
-            while (board[upSign.x][upSign.y]) {
-                upSign.x -= 1;
-                inline++;
-            }
+        currentPoint.x = startingPoint.x - xModifier;
+        currentPoint.y = startingPoint.y - yModifier;
+
+        isInBoard = currentPoint.x >= 0 && currentPoint.x < height;
+        while (isInBoard && board[currentPoint.x][currentPoint.y] === currentPlayer.sign) {
+            currentPoint.x -= xModifier;
+            currentPoint.y -= yModifier;
+            isInBoard = currentPoint.x >= 0 && currentPoint.x < height;
+            inLine++;
         }
 
-        const downSign = { x: startingPoint.x + 1, y: startingPoint.y };
-        if (downSign.x < height) {
-            console.log();
-            while (board[downSign.x][downSign.y]) {
-                downSign.x += 1;
-                inline++;
-            }
-        }
-        inline = 1;
-
-        const leftUpSign = { x: startingPoint.x - 1, y: startingPoint.y - 1 };
-        if (upSign.x >= 0) {
-            while (board[leftUpSign.x][leftUpSign.y]) {
-                leftUpSign.x -= 1;
-                leftUpSign.y -= 1;
-                inline++;
-            }
-        }
-
-        const rightDown = { x: startingPoint.x + 1, y: startingPoint.y + 1 };
-        if (rightDown.x < height) {
-            while (board[rightDown.x][rightDown.y]) {
-                rightDown.x += 1;
-                rightDown.y += 1;
-                inline++;
-            }
-        }
-
-        if (inline >= 5) {
-            return true;
-        }
-        return false;
+        return inLine >= 5;
     };
 
     return (
         <div style={{ height: "100%" }}>
+            <h1>
+                {currentPlayer.name}: {currentPlayer.sign}
+            </h1>
             {board &&
                 board.map((row, rowIndex) => {
                     return (
-                        <div
-                            key={rowIndex}
-                            style={{ display: "flex", flexDirection: "row" }}
-                            onClick={() => console.log("verify")}
-                        >
+                        <div key={rowIndex} style={{ display: "flex", flexDirection: "row" }}>
                             {row.map((column, columnIndex) => {
                                 return (
                                     <Cell
@@ -93,7 +85,12 @@ export default function Board({ width = 10, height = 10 }) {
                                         rowIndex={rowIndex}
                                         columnIndex={columnIndex}
                                         playerMove={playerMove}
-                                        style={{ height: `${90 / height}vh`, width: `${100 / height}vh` }}
+                                        playerSign={currentPlayer.sign}
+                                        style={{
+                                            height: `${90 / height}vmin`,
+                                            width: `${90 / height}vmin`,
+                                            fontSize: `${60 / (90 / height)}vmin`,
+                                        }}
                                     />
                                 );
                             })}
--- src/components/game/Cell.js
+++ src/components/game/Cell.js
@@ -1,22 +1,17 @@
 import React, { useState } from "react";
 
-export default function Cell({ rowIndex, columnIndex, style, playerMove }) {
-    const [row, setRow] = useState(rowIndex);
-    const [column, setColumn] = useState(columnIndex);
+export default function Cell({ rowIndex, columnIndex, style, playerMove, playerSign }) {
     const [sign, setSign] = useState("");
 
     const placeSign = () => {
         if (!sign) {
-            setSign("X");
+            setSign(playerSign);
             playerMove({ x: rowIndex, y: columnIndex });
         }
     };
 
     return (
-        <div
-            style={{ border: "1px solid #000", display: "grid", placeItems: "center", fontSize: "36px", ...style }}
-            onClick={placeSign}
-        >
+        <div style={{ border: "1px solid #000", display: "grid", placeItems: "center", ...style }} onClick={placeSign}>
             {sign}
         </div>
     );
