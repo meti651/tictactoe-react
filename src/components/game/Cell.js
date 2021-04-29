27c5f834cf4fa62a3184bcd934420a191aa31916
meti651 <meti651@gmail.com>
2021-04-30 00:11:53 +0200

    refactor, implement game over, implement restart in Cell

--- src/components/game/Cell.js
+++ src/components/game/Cell.js
@@ -1,14 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import BoardPoint from "../../utility/game/classes/Point";
 
-export default function Cell({ rowIndex, columnIndex, style, playerMove, playerSign }) {
+export default function Cell({ value, rowIndex, columnIndex, style, playerMove, playerSign, isGameRunning }) {
     const [sign, setSign] = useState("");
 
     const placeSign = () => {
-        if (!sign) {
+        if (!sign && isGameRunning) {
             setSign(playerSign);
-            playerMove({ x: rowIndex, y: columnIndex });
+            playerMove(new BoardPoint(rowIndex, columnIndex));
         }
     };
+
+    useEffect(() => {
+        setSign(value ? value : "");
+    }, [value]);
 
     return (
         <div style={{ border: "1px solid #000", display: "grid", placeItems: "center", ...style }} onClick={placeSign}>
