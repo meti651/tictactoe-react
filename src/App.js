0a185f2bfbc44c7a4d2d34cedb1d1420cdbd9b67
meti651 <meti651@gmail.com>
2021-04-30 00:08:48 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
 import Board from "./components/game/Board";
 
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Menu from "./components/menu/Menu";
+
 function App() {
     return (
         <div className="App">
-            <Board />
+            <Router>
+                <Switch>
+                    <Route path="/" exact>
+                        <Menu />
+                    </Route>
+                    <Route path="/board">
+                        <Board />
+                    </Route>
+                </Switch>
+            </Router>
         </div>
     );
 }
